c6b09eb3db21e32f5ee246c62c4c04def8f80478
s245045-fuyu <s245045@matsu.shimane-u.ac.jp>
2026-09-02 15:45:57 +0900

    メッセージを追加

--- app/javascript/custom/password_toggle.js
+++ app/javascript/custom/password_toggle.js
@@ -1,3 +1,4 @@
+/* パスワードを表示・非表示のボタンをクリック時に設定する関数 */
 function bindPasswordToggles() {
   document.querySelectorAll(".toggle_password").forEach(function(button) {
     if (button.dataset.bound === "true") return;
@@ -26,6 +27,7 @@ function bindPasswordToggles() {
   });
 }
 
+/*turboと通常のロードの両方に対応*/ 
 document.addEventListener("turbo:load", function() {
   bindPasswordToggles();
 });
